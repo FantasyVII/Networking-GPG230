0768b815f269b7773470a402c7e430c7aad14751
Mustafa Sibai <contact@m-sibai.com>
2020-10-07 19:19:44 +0400

    Created both server and client with a way run them using arg in cli

--- Network/Program.cs
+++ Network/Program.cs
@@ -10,7 +10,7 @@ namespace Network
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Server()
         {
             Socket listenerSocket;
             Socket acceptSocket;
@@ -22,6 +22,37 @@ namespace Network
 
             acceptSocket = listenerSocket.Accept();
             Console.WriteLine("Connection established...");
+        }
+
+        static void Client()
+        {
+            Socket mainSocket;
+            mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Attempting to connect to server...");
+
+            mainSocket.Connect(new IPEndPoint(IPAddress.Parse("5.30.132.84"), 27016));
+            Console.WriteLine("Connection established...");
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "-server":
+                        Server();
+                        break;
+                    case "-client":
+                        Client();
+                        break;
+                    default:
+                        Console.WriteLine("Cannot run program with " + args[0] + " as an argument");
+                        break;
+                }
+            }
+            else
+                Console.WriteLine("Cannot run program with no arugments. Please run the program with -server or -client as arugments");
         }
     }
 }
