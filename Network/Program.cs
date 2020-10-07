ffcbeac14d7eeb6c4ab6cb65468afa3297ae0094
Mustafa Sibai <contact@m-sibai.com>
2020-10-07 19:18:15 +0400

    Created Server

---
+++ Network/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket;
+            Socket acceptSocket;
+
+            listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenerSocket.Bind(new IPEndPoint(IPAddress.Loopback, 27016));
+            Console.WriteLine("Waiting for incoming connection...");
+            listenerSocket.Listen(5);
+
+            acceptSocket = listenerSocket.Accept();
+            Console.WriteLine("Connection established...");
+        }
+    }
+}
