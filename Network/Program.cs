f5419b22ac86a562903706b1a50c2790f392f3f9
Mustafa Sibai <contact@m-sibai.com>
2020-10-14 20:57:04 +0400

    Added the ability for multiple clients to connect to the server and for the server to send all messages received from clients to all clients

--- Network/Program.cs
+++ Network/Program.cs
@@ -21,13 +21,14 @@ namespace Network
             listenerSocket.Bind(new IPEndPoint(IPAddress.Any, port));
             Console.WriteLine("Waiting for incoming connection...");
 
-            byte[] receivingBuffer = new byte[100];
+            byte[] receivingBuffer = new byte[150];
             string message = "";
 
             while (true)
             {
                 listenerSocket.Listen(10);
 
+                //--------------------- LISTINING FOR INCOMING CONNECTIONS --------------------------
                 try
                 {
                     Socket acceptSocket = listenerSocket.Accept();
@@ -39,14 +40,36 @@ namespace Network
                     if (e.SocketErrorCode != SocketError.WouldBlock)
                         Console.WriteLine(e);
                 }
+                //--------------------- LISTINING FOR INCOMING CONNECTIONS --------------------------
 
                 for (int i = 0; i < client.Count; i++)
                 {
                     Array.Clear(receivingBuffer, 0, receivingBuffer.Length);
                     try
                     {
+                        //--------------------- RECEIVING DATA --------------------------
                         client[i].Receive(receivingBuffer);
                         message = Encoding.ASCII.GetString(receivingBuffer);
+                        //--------------------- RECEIVING DATA --------------------------
+
+                        //------------ SENDING DATA ------------------
+                        for (int j = 0; j < client.Count; j++)
+                        {
+                            try
+                            {
+                                byte[] sendingBuffer = Encoding.ASCII.GetBytes(message);
+                                client[j].Send(sendingBuffer);
+                            }
+                            catch (SocketException e)
+                            {
+                                if (e.SocketErrorCode != SocketError.WouldBlock)
+                                {
+                                    Console.WriteLine(e);
+                                }
+                            }
+                        }
+                        //------------ SENDING DATA ------------------
+
                         Console.WriteLine(message);
                     }
                     catch (SocketException e)
@@ -56,6 +79,7 @@ namespace Network
                             if (e.SocketErrorCode == SocketError.ConnectionAborted || e.SocketErrorCode == SocketError.ConnectionReset)
                             {
                                 Console.WriteLine("Client Disconnected...");
+                                client[i].Close();
                                 client.RemoveAt(i);
                             }
                             else
@@ -65,23 +89,8 @@ namespace Network
                         }
                     }
                 }
-
-                //--------------------- RECEIVING DATA --------------------------
-
-                //--------------------- RECEIVING DATA --------------------------
-
-
-                /*
-                    //--------------------- SENDING DATA --------------------------
-                    message = Console.ReadLine();
-                    byte[] sendingBuffer = Encoding.ASCII.GetBytes(message);
-                    acceptSocket.Send(sendingBuffer);
-                    //--------------------- SENDING DATA --------------------------
-                */
-
             }
 
-            // acceptSocket.Close();
             //listenerSocket.Close();
         }
 
@@ -89,42 +98,141 @@ namespace Network
         {
             Socket mainSocket;
             mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            // mainSocket.Blocking = false;
+            mainSocket.Blocking = false;
             Console.WriteLine("Attempting to connect to server...");
 
             IPAddress ip;
             bool isIpValid = IPAddress.TryParse(ipOrHostName, out ip);
 
-            if (isIpValid)
-                mainSocket.Connect(new IPEndPoint(ip, port));
-            else
-                mainSocket.Connect(ipOrHostName, port);
-
-            Console.WriteLine("Connection established...");
-            Console.WriteLine("Please type the message you want to send");
-
             byte[] receivingBuffer = new byte[150];
-            string message = "";
+            string sendingMessage = "";
+            string receiveMessage = "";
+
+            bool connectedToServer = false;
+            int cursorIndex = 0;
 
             while (true)
             {
+                if (!connectedToServer)
+                {
+                    try
+                    {
+                        connectedToServer = true;
+                        if (isIpValid)
+                            mainSocket.Connect(new IPEndPoint(ip, port));
+                        else
+                            mainSocket.Connect(ipOrHostName, port);
+
+                        Console.WriteLine("Connection established...");
+                        Console.WriteLine("Please type the message you want to send");
+                    }
+                    catch (SocketException e)
+                    {
+                        if (e.SocketErrorCode != SocketError.WouldBlock)
+                        {
+                            if (e.SocketErrorCode == SocketError.ConnectionAborted || e.SocketErrorCode == SocketError.ConnectionReset)
+                            {
+                                Console.WriteLine("Server Disconnected...");
+                                mainSocket.Close();
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine(e);
+                            }
+                        }
+                    }
+                }
+
                 //--------------------- SENDING DATA --------------------------
-                message = nickname + ": " + Console.ReadLine();
-                byte[] sendingBuffer = Encoding.ASCII.GetBytes(message);
-                mainSocket.Send(sendingBuffer);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(false);
+
+                    if (key.Key == ConsoleKey.Backspace)
+                    {
+                        if (cursorIndex > 0)
+                        {
+                            sendingMessage = sendingMessage.Remove(sendingMessage.Length - 1);
+                            Console.Write(' ');
+                            Console.Write(key.KeyChar);
+                            cursorIndex--;
+                        }
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        string finalMessage = nickname + ": " + sendingMessage;
+
+                        try
+                        {
+                            byte[] sendingBuffer = Encoding.ASCII.GetBytes(finalMessage);
+                            mainSocket.Send(sendingBuffer);
+                            sendingMessage = "";
+                            cursorIndex = 0;
+                        }
+                        catch (SocketException e)
+                        {
+                            if (e.SocketErrorCode != SocketError.WouldBlock)
+                            {
+                                if (e.SocketErrorCode == SocketError.ConnectionAborted || e.SocketErrorCode == SocketError.ConnectionReset)
+                                {
+                                    Console.WriteLine("Server Disconnected...");
+                                    mainSocket.Close();
+                                    return;
+                                }
+                                else
+                                {
+                                    Console.WriteLine(e);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        sendingMessage += key.KeyChar;
+                        cursorIndex++;
+                    }
+                }
                 //--------------------- SENDING DATA --------------------------
 
-                /*
                 //--------------------- RECEIVING DATA --------------------------
-                Array.Clear(receivingBuffer, 0, receivingBuffer.Length);
-                mainSocket.Receive(receivingBuffer);
-                message = Encoding.ASCII.GetString(receivingBuffer);
-                Console.WriteLine(message);
-                //--------------------- RECEIVING DATA --------------------------
-                */
+                try
+                {
+                    Array.Clear(receivingBuffer, 0, receivingBuffer.Length);
+                    mainSocket.Receive(receivingBuffer);
+                    receiveMessage = Encoding.ASCII.GetString(receivingBuffer);
+
+                    ClearLine();
+                    Console.WriteLine(receiveMessage);
+                    Console.Write(sendingMessage);
+                }
+                catch (SocketException e)
+                {
+                    if (e.SocketErrorCode != SocketError.WouldBlock)
+                    {
+                        if (e.SocketErrorCode == SocketError.ConnectionAborted || e.SocketErrorCode == SocketError.ConnectionReset)
+                        {
+                            Console.WriteLine("Server Disconnected...");
+                            mainSocket.Close();
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine(e);
+                        }
+                    }
+                }
             }
+            //--------------------- RECEIVING DATA --------------------------
 
             //mainSocket.Close();
+        }
+
+        static void ClearLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
         }
 
         static void Main(string[] args)
